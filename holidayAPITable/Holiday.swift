6850e4ea7cf29641aba476a494a2cf64997e226e
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-12-01 22:21:52 +0000

    Files added, API decoder info added.

--- holidayAPITable/Holiday.swift
+++ holidayAPITable/Holiday.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 
+//Look into API for key names
+// Decodable decodes information from JSON/API
+// String has to be same name as API e.g. ios
+
+struct HolidayResponse:Decodable {
+    var response:Holidays
+}
+
+struct Holidays:Decodable {
+    var holidays:[HolidayDetail]
+}
+
+struct HolidayDetail:Decodable {
+    var name:String
+    var date:DateInfo
+}
+
+struct DateInfo:Decodable {
+    var ios:String
+}
---
+++ holidayAPITable/HolidayRequest.swift
@@ -0,0 +1,10 @@
+//
+//  HolidayRequest.swift
+//  holidayAPITable
+//
+//  Created by Rachel Saunders on 01/12/2019.
+//  Copyright © 2019 Rachel Saunders. All rights reserved.
+//
+
+import Foundation
+
