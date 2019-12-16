501c1b6e9e829415063ff5dce6a109e4dfb49820
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-12-16 20:09:17 +0000

    Complete but with 3 Errors. Unable to build. Unable to run.

--- holidayAPITable/HolidaysTableViewController.swift
+++ holidayAPITable/HolidaysTableViewController.swift
@@ -14,6 +14,15 @@ class HolidaysTableViewController: UITableViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var listOfHolidays = [HolidayDetail]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.navigationItem.title = "\(self.listOfHolidays.count) Holidays found"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
@@ -26,11 +35,15 @@ class HolidaysTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return listOfHolidays.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let holiday = listOfHolidays[indexPath.row]
+        
+        cell.textLabel?.text = holiday.name
+        cell.detailTextLabel?.text = holiday.date.iso
         
         return cell
     }
@@ -41,7 +54,16 @@ class HolidaysTableViewController: UITableViewController {
 
 extension HolidayTableViewController : UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
+        guard let searchBarText = searchBar.text else {return}
+        let holidayRequest = HolidayRequest(countryCode: searchBarText)
+        holidayRequest.getHolidays {[weak self] result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let holidays):
+                self?.listOfHolidays = holidays
+            }
+        }
         
     }
 }
