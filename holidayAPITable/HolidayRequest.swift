bfa63779c9c48d5ae323288ca8e9889d3138f972
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-12-16 19:57:49 +0000

    API Info added

--- holidayAPITable/HolidayRequest.swift
+++ holidayAPITable/HolidayRequest.swift
@@ -8,3 +8,48 @@
 
 import Foundation
 
+enum HolidayError:Error {
+    case noDataAvailable
+    case cannotProcessData
+}
+
+struct HolidayRequest {
+    let resourceURL:URL
+    let API_KEY = "43f3d4d51a636efa9fa986daca5973a36cc6fc07"
+    
+    
+    init(countryCode:String) {
+        
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy"
+        let currentYear = format.string(from: date)
+        
+        
+        let resourceString = "https://calendarific.com/api/v2/holidays?api_key=\(API_KEY)&country=\(countryCode)&year=\(currentYear)"
+        guard let resourceURL = URL(string: resourceString) else {fatalError()}
+        
+        self.resourceURL = resourceURL
+        
+    }
+    
+    func getHolidays (completion: @escaping(Result<[HolidayDetail], HolidayError>) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: resourceURL) {data, _, _ in
+            guard let jsonData = data else {
+                completion(.failure(.noDataAvailable))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let holidayResponse = try decoder.decode(HolidayResponse.self, from: jsonData)
+                let holidayDetails = holidayResponse.response.holidays
+                completion(.success(holidayDetails))
+            }catch{
+                completion(.failure(.cannotProcessData))
+            }
+            
+        }
+        dataTask.resume()
+    }
+    
+}
